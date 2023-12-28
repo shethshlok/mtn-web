1cf9e3e1eb5ac21d1219a1781e51b6e0123e84db
Shlok Sheth <ss3756@nau.edu>
2023-12-27 17:33:04 -0700

    Update HeroPage styling and add AgePrompt component

---
+++ src/components/AgePrompt.js
@@ -0,0 +1,32 @@
+// AgeVerificationModal.js
+
+import React from 'react';
+import './AgePrompt.css';
+// import { useHistory } from 'react-router-dom';
+
+const AgePrompt = ({ onConfirm, onReject }) => {
+
+  
+    const handleLeftClick = () => {
+        // Handle left half click, e.g., redirect to page1
+        console.log('left');
+        onConfirm();
+      };
+    
+      const handleRightClick = () => {
+        // Handle right half click, e.g., redirect to page2
+        console.log('right');
+        // stop the user accessing thw website
+        onReject();
+        
+      };
+  
+  
+    return (
+    <div className="modal-overlay">
+        <div className='agePrompt' onClick={(e) => e.clientX <= window.innerWidth / 2 ? handleLeftClick() : handleRightClick()} />
+    </div>
+  );
+};
+
+export default AgePrompt;
--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -5,19 +5,43 @@ import { NavLink } from 'react-router-dom';
 import './HeroPage.css'; // Import your CSS file for styling
 import { FaSearch } from "react-icons/fa";
 import { RiArrowDropDownLine } from "react-icons/ri";
+import { useState } from 'react'; // Import useState hook
+import AgePrompt from './AgePrompt'; // Import the new component
 
 const HeroPage = () => {
+  
+  
+  const [showAgeModal, setShowAgeModal] = useState(true);
+
+  const handleConfirmAge = () => {
+    // Add logic to handle age confirmation
+    setShowAgeModal(false);
+  };
+
+  const handleRejectAge = () => {
+    // Add logic to handle age rejection
+    setShowAgeModal(false);
+  };
+  
+  
   return (
     <div className='heroPage'>
+      {/* Age Verification Modal */}
+      {showAgeModal && (
+        <AgePrompt
+          onConfirm={handleConfirmAge}
+          onReject={handleRejectAge}
+        />
+      )}
     
         <div className="header">
-
-            <div className="logo-container"></div>
 
             <div className="search-container">
             {/* <input className='searchBox' type="text" placeholder="Search" /> */}
                 <FaSearch />
             </div>
+
+            <div className="logo-container"></div>
             <div className='cart-container'>
             </div>
         </div>
@@ -33,7 +57,7 @@ const HeroPage = () => {
             <div className='down-arrow'>Beer<RiArrowDropDownLine /> </div>
             </div>
             <div className='lower-header-item'>
-            <div className='down-arrow'>Pick up<RiArrowDropDownLine /> </div>
+            <div className='down-arrow'>Offers<RiArrowDropDownLine /> </div>
             </div>
         </div>
     </div>
